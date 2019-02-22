2a288baa480ef50b083c0bbaffef8594d37acfb1
lcrobert <lcrobert.rocket@gmail.com>
2019-02-23 01:37:54 +0800

    code refractory and rewrite fn: ChkTFRecordsFile

--- ReadTFRecords.py
+++ ReadTFRecords.py
@@ -4,7 +4,6 @@ As Title, and handling the input dataset pipeline
 """
 import os
 import tensorflow as tf
-import matplotlib.pyplot as plt 
 #from datetime import datetime
 
 def counter_TFRecord_datasize(path):
@@ -12,53 +11,6 @@ def counter_TFRecord_datasize(path):
     for record in tf.python_io.tf_record_iterator(path):
         counts += 1 
     return counts 
-
-def chk_TFRecords_file(filepath, batch_size=1, plot=False):   
-    
-    def parse_single_example(record):         
-        img_features = tf.parse_single_example(
-                       record,
-                       features={'label': tf.FixedLenFeature([], tf.int64),
-                                 'image': tf.FixedLenFeature([], tf.string),
-                                 'height': tf.FixedLenFeature([], tf.int64),
-                                 'width': tf.FixedLenFeature([], tf.int64),
-                                 'filename': tf.FixedLenFeature([], tf.string),                             
-                                 })          
-        image = tf.decode_raw(img_features['image'], tf.uint8)
-        label = tf.cast(img_features['label'], tf.uint8) 
-        filename = img_features['filename'] #need .decode('utf8')
-        height = img_features['height']       
-        width = img_features['width']                      
-        return image, label, filename, height, width  
-
-    # Settiing TFRecord structure     
-    dataset = tf.data.TFRecordDataset(filepath)
-    dataset = dataset.map(parse_single_example)  # Parse the record into tensors.    
-    # Setting Read data in batch mode 
-    dataset = dataset.batch(batch_size)
-    iterator = dataset.make_initializable_iterator()
-    # Start to read data
-    sess = tf.Session() 
-    sess.run(iterator.initializer)
-    images, labels, filenames, heights, widths = sess.run(iterator.get_next())
-    #print (labels, filenames, heights, widths)
-    sess.close()
-    
-    # Reshape images to orignal shape due to that is a 1-D np array 
-    images = [ images[i].reshape((heights[0], widths[0], 3)) for i in range(batch_size)  ]
-
-    # Count data points of this file
-    data_counts = counter_TFRecord_datasize(filepath)
-
-    # Plot images if plot is True 
-    if plot:        
-       plt.figure(filepath.replace("\\","/").split("/")[-1]) 
-       for i in range(batch_size):        
-           plt.subplot(batch_size,1,i+1)
-           plt.imshow(images[i])
-       
-    return {'DataSize': data_counts,'labels':labels, 'filenames':filenames, 
-            'imgsize': [set(heights),set(widths)]}  
 
 
 class InputDataPipeline(object):
@@ -69,10 +21,9 @@ class InputDataPipeline(object):
         self.batch_size = batch_size
         self.image_shape = image_shape
         self.label_size = label_size
-        self.epoch = epoch
-           
-        self.init_iterator()     
-
+        self.epoch = epoch           
+        self.init_iterator()
+        
     def init_iterator(self):
         with tf.name_scope("InputData_iterator"):
           with tf.name_scope("train_readTFRecords"):
@@ -81,7 +32,7 @@ class InputDataPipeline(object):
             self.vali_dataset, self.vali_iterator = self.Read_TFRecords_file(self.inputdata_path['vali'],"vali",self.batch_size['vali'])
           with tf.name_scope("test_readTFRecords"):
             self.test_dataset, self.test_iterator = self.Read_TFRecords_file(self.inputdata_path['test'],"test",self.batch_size['test'])
-        
+          
           # Make feedable iterator
           self.inputdata_handle = tf.placeholder(tf.string, shape=[],name='inputdata_handle')
           self.iterator = tf.data.Iterator.from_string_handle(
@@ -92,7 +43,6 @@ class InputDataPipeline(object):
         
         for x in [self.train_iterator, self.vali_iterator, self.test_iterator]:
             self.sess.run(x.initializer)
-            #print (self.batch_size)
 
     def parse_single_example(self, record):
         img_features = tf.parse_single_example(
@@ -106,10 +56,14 @@ class InputDataPipeline(object):
         
         image = tf.decode_raw(img_features['image'], tf.uint8) #1-D
         label = tf.cast(img_features['label'], tf.uint8) 
-        filename = img_features['filename'] #need .decode('utf8') 
-        return image, label, filename         
-    
-        
+        filename = img_features['filename'] #need .decode('utf8')
+        height = img_features['height']       
+        width = img_features['width']         
+        if self.image_shape != '':
+           return image, label, filename  
+        else:
+           return image, label, filename, height, width    
+            
     def Read_TFRecords_file(self, filename, data_type, batchsize, image_shape=0, sh_buffer=0):
         dataset = tf.data.TFRecordDataset(filename)
         dataset = dataset.map(self.parse_single_example) 
@@ -120,28 +74,27 @@ class InputDataPipeline(object):
         the_iterator = dataset.make_initializable_iterator()
         return dataset, the_iterator
 
-
     def feeder(self, selected_iterator):
         handle = self.sess.run(selected_iterator.string_handle())
         return self.sess.run([self.batch_image, self.batch_label],
                              feed_dict={self.inputdata_handle: handle}) 
       
 
-
-
-class InputData(object):
+# Single InputData set
+class InputData(InputDataPipeline):
 
     def __init__(self, sess, inputdata_path, batch_size, image_shape, label_size):
         self.sess = sess
         self.inputdata_path = inputdata_path
-        self.batch_size = batch_size
+        if batch_size == None: 
+           self.batch_size = counter_TFRecord_datasize(inputdata_path)
+        else:   
+           self.batch_size = batch_size
         self.image_shape = image_shape
-        self.label_size = label_size
-           
+        self.label_size = label_size           
         self.init_iterator()     
 
     def init_iterator(self):
-
         self.the_dataset, self.the_iterator = self.Read_TFRecords_file(self.inputdata_path,self.batch_size)
    
         self.inputdata_handle = tf.placeholder(tf.string, shape=[],name='inputdata_handle')
@@ -149,26 +102,9 @@ class InputData(object):
                           self.inputdata_handle, self.the_dataset.output_types)
         self.batch_image, self.batch_label, self.batch_filename = self.iterator.get_next()
         self.batch_image = tf.reshape(self.batch_image, [-1, self.image_shape[0], self.image_shape[1], 3])
-        self.batch_label = tf.one_hot(self.batch_label, self.label_size)
-        
+        self.batch_label = tf.one_hot(self.batch_label, self.label_size)        
         self.sess.run(self.the_iterator.initializer)
-
-    def parse_single_example(self, record):
-        img_features = tf.parse_single_example(
-                       record,
-                       features={'label': tf.FixedLenFeature([], tf.int64),
-                                 'image': tf.FixedLenFeature([], tf.string),
-                                 'height': tf.FixedLenFeature([], tf.int64),
-                                 'width': tf.FixedLenFeature([], tf.int64),
-                                 'filename': tf.FixedLenFeature([], tf.string),                             
-                                 })
-        
-        image = tf.decode_raw(img_features['image'], tf.uint8) #1-D
-        label = tf.cast(img_features['label'], tf.uint8) 
-        filename = img_features['filename'] #need .decode('utf8') 
-        return image, label, filename         
-    
-        
+                  
     def Read_TFRecords_file(self, filename, batchsize):
         dataset = tf.data.TFRecordDataset(filename)
         dataset = dataset.map(self.parse_single_example)    
@@ -176,10 +112,68 @@ class InputData(object):
         the_iterator = dataset.make_initializable_iterator()
         return dataset, the_iterator
 
-    def feeder(self, selected_iterator):
-        handle = self.sess.run(selected_iterator.string_handle())
-        return self.sess.run([self.batch_image, self.batch_label],
-                             feed_dict={self.inputdata_handle: handle}) 
+
+# Chk contents in the TFRecordsFile
+class ChkTFRecordsFile(InputData): 
+     
+    def __init__(self, filepath, batch_size=1):
+        self.filepath = filepath
+        self.image_shape = '' # fn:parse_single_example used because unknown w h
+        self.datasize = counter_TFRecord_datasize(filepath)
+        self.sess = tf.Session() 
+        self.init_iterator(batch_size) 
+        
+    def init_iterator(self, batch_size):
+        self.batch_size = batch_size 
+        self.the_dataset, self.the_iterator = self.Read_TFRecords_file(self.filepath, self.batch_size)                    
+        self.sess.run(self.the_iterator.initializer) ##with self.sess.as_default():
+   
+    def get_data(self, new_batch_size=0, show_info=False, plot=False):
+        if new_batch_size > 0: self.init_iterator(new_batch_size)           
+        images, labels, filenames, heights, widths = self.sess.run(self.the_iterator.get_next())
+        images = [ images[i].reshape((heights[0], widths[0], 3)) for i in range(self.batch_size)  ] 
+        self.data = {'img': images,
+                     'label':labels, 
+                     'filename':filenames, 
+                     'imgsize': [set(heights),set(widths)],
+                     'datasize': self.datasize}        
+        if show_info:
+           print (self.filepath.replace("\\","/").split("/")[-1])             
+           for key,value in self.data.items(): 
+               print (key ,':', value) if key != 'img' else print (key ,':', 'skip') 
+           print ('----------------------------------')    
+        if plot : self.plot_imgs(images)        
+        return self.data
+
+    def get_all_data(self):       
+        self.data = self.get_data(new_batch_size=self.datasize)
+        return self.data
+
+    def count_label(self, labels, only_print_result=True):
+        import collections
+        label_counts = collections.Counter(labels)
+        label_counts = sorted(label_counts.items()) 
+        if only_print_result:
+           for i in label_counts : print (i)
+        else:    
+           for i in label_counts : print (i)
+           return label_counts
+    
+    def get_label_distribution(self):
+        self.data = self.get_all_data()
+        self.label_counts = self.count_label(self.data['label'],only_print_result=False)
+        return self.label_counts  
+
+    def plot_imgs(self, images): 
+        img_number = len(images) 
+        if img_number < 5:
+           import matplotlib.pyplot as plt            
+           plt.figure(self.filepath.replace("\\","/").split("/")[-1]) 
+           for i in range(img_number):        
+               plt.subplot(img_number,1,i+1)
+               plt.imshow(images[i])            
+        else:
+           print ("Could not plot OVER 4 images - Please re-select images OR re-run get_data(new_batch_size=4)") 
 
 
 ###############################################################################
@@ -189,16 +183,19 @@ if __name__ == '__main__':
   vali_path = os.path.join(input_main_path,'MyDataV2_vali.tfrecords')   
   test_path = os.path.join(input_main_path,'MyDataV2_test.tfrecords') 
 
-  for filepath in [train_path,vali_path,test_path]:
-      filename = filepath.replace("\\","/").split("/")[-1]
-      datasize = counter_TFRecord_datasize(filepath)
-      print (filename,':\n','size :',datasize)
-      
-  """    
-  my_data = chk_TFRecords_file(train_path,batch_size=4,plot=True)
-  for key,value in my_data.items(): 
-      print (key ,':', value)
-  """  
+  file = ChkTFRecordsFile(test_path,batch_size=4)
+  
+  data_all = file.get_all_data() 
+  #file.plot_imgs(data_all['img'][0:3])
+  labels_all = file.get_label_distribution()  
+  data = file.get_data(new_batch_size=4, show_info=True, plot=True)  
+  data2 = file.get_data(show_info=True, plot=True)  
+
+
+
+
+  
+
 
 
 
