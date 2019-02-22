ebbb5e4a039c5a86226e20cf5c19d9fb0ae5d298
lcrobert <lcrobert.rocket@gmail.com>
2019-02-23 02:33:30 +0800

    rewrite fn: ModelToPB to match CnnModelClass.py

--- s5_DeployPbFile.py
+++ s5_DeployPbFile.py
@@ -7,39 +7,34 @@ import tensorflow as tf
 
 
 def ModelToPB(model_path,pb_path):
-    from ModelTools import RestoreModelVariable, FreezeGraphToPB
+    from ModelTools import RestoreModel, FreezeGraphToPB
     tf.reset_default_graph()
-    sess = tf.Session() 
-    x_img, y_label, keep_prob, training, output_propb, output_label = RestoreModelVariable(sess,model_path)
-    FreezeGraphToPB(sess,pb_path,output_node=[output_label,output_propb])
+    sess = tf.Session()
+    sess, x_img, y_label, keep_prob, training, output_prob, output_label, train_step = RestoreModel(sess,model_path)     
+    FreezeGraphToPB(sess,pb_path,output_node=[output_label,output_prob])
     sess.close()
   
 
-
-if __name__ == '__main__':
-   # Model to .pb file  
+if __name__ == '__main__':   
    model_path = os.path.join(os.getcwd(),'OutputModel',
-                                         'colab_201901040210',                                        
-                                         'MyDataV1_m2')     
-   pbfile_path = model_path+"_test.pb"
+                                         'colab_201902100257',                                        
+                                         'MyDataV2_m4')      
+   pbfile_path = model_path+".pb"
+   test_path = os.path.join(os.getcwd(),'InputData',
+                         'MyDataV2_test.tfrecords') 
+   # Model to .pb file  
    ModelToPB(model_path,pbfile_path)
    
-   """# Test .pb file   
-   from QuickFunctions import PredMyRealImgsPB
-   PredMyRealImgsPB(pbfile_path)
-   """
+   # Chk .pb file   
+   from QuickFunctions import PredMyRealImgs, PredTestSet
+   PredMyRealImgs(pbfile_path)
+
+   # Chk .pb file with test set
+   PredTestSet(pbfile_path,test_path,save=False)
+
    
-   """
-   # Read .pb file and get tensor
-   from ModelTools import LoadGraphFromPB
-   tf.reset_default_graph()
-   graph = LoadGraphFromPB(pbfile_path)
-   x_img = graph.get_tensor_by_name('import/InputData/x_img:0')
-   keep_prob =graph.get_tensor_by_name('import/dropout:0')
-   output_propb = graph.get_tensor_by_name('import/Output-Classification-layer/output_softmax/Softmax:0')
-   output_label = graph.get_tensor_by_name('import/ArgMax:0')
-   """
-   #sess = tf.Session()
+   
+
    
    
 
